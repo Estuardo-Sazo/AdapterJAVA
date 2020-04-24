5ad4af69a92ebbb83eeda0bfa47e30d34b79702e
Estuardo-Sazo <estuardosazo1206@gmail.com>
2020-04-24 14:17:02 -0600

    Create PersonaNueva.java

---
+++ PatronAdapter/src/patronadapter/PersonaNueva.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package patronadapter;
+
+/**
+ *
+ * @author Jaime Sazo
+ */
+public interface PersonaNueva {
+    
+    public String getNombre();
+    public void setNombre(String nombre);
+    public int getEdad();
+    public void setEdad(int edad);
+    
+}
